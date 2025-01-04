e982a82d110b4324a0493e253c40529c54029c04
Hammad <mohammedhammad@gmail.com>
2025-01-04 10:31:08 +0800

    added port

--- app.js
+++ app.js
@@ -186,6 +186,12 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
+
+
+// Start the server
 // Start the server
 const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 module.exports = app;
